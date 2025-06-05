b6270f39dd36724468642f9f40986915321ca992
SandaruwanWeerawardhana <weerawadanasw@gmail.com>
2025-06-05 15:38:50 +0530

    Log requests using morgan

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-
 import express from 'express';
 import cors from 'cors';
 import { dbInit } from './src/config/db.js';
 import feedbackRouter from './src/routes/feedbackRoute.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 await dbInit();
 
